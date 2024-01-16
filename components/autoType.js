c826c9b14032a5098941a7eb07e525520e6b0187
hertuq0910 <lchernandez151@misena.edu.co>
2024-01-16 13:13:37 -0500

    Primer commit

---
+++ components/autoType.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".auto-input", {
+  strings: [
+    "Desarrollador Fullstack",
+    "Desarrollador Back-end",
+    "Fullstack Developer",
+    "Back-end Developer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
